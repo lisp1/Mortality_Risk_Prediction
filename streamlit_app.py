4c7fcb343d2136afeb67a46b5921c125740316dd
lisp1 <44103252+lisp1@users.noreply.github.com>
2025-01-24 18:03:47 +0800

    Update streamlit_app.py to add streamlit caching

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,7 @@ def convert_strings_to_paths(obj):
     # Add more conditions if necessary
     return obj
 
+@st.cache_data
 def loadsupport():
 	with open('selected_models2.pkl', 'rb') as f:
 	    loaded_vars = pickle.load(f)
